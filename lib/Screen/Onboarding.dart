23fbd7a1b84a7b209e81d1ea4e464be22afef7a9
Chetan <royalstar2586@gmail.com>
2025-02-10 22:01:23 +0530

    setup

---
+++ lib/Screen/Onboarding.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: size.height/2,
+            child: Image.asset('assets/img/onboard.jpg'),
+          )
+        ],
+      ),
+    );
+  }
+}
