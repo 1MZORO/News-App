c9585e6df4dee17521bdf81215153c336cf1bc5c
Chetan <royalstar2586@gmail.com>
2025-02-11 00:33:29 +0530

    HOttest News

--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -5,12 +5,177 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('data')
-        ],
+      backgroundColor: Color.fromARGB(244, 255 , 255, 255),
+      body: Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'TIME ',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue),
+                ),
+                Text(
+                  'NEWS',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Hottest News',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 23),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 330,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Material(
+                  elevation: 3,
+                  borderRadius: BorderRadius.circular(15),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10,left: 10),
+                    child: Container(
+                      decoration: BoxDecoration(color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(15))
+                      ),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Image.asset(
+                                  'assets/img/news1.jpg',
+                                  fit: BoxFit.cover,
+                                  height: 150,
+                                  width: size.width / 1.8,
+                                ),
+                              )),
+                          SizedBox(height: 5,),
+                          Container(
+                            width: size.width / 1.8,
+                            child: Text(
+                              'I am an flutter app developer i am creating an news app ',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 19),
+                            ),
+                          ),
+                          Container(
+                            width: size.width / 1.8,
+                            child: Text(
+                              'I am an flutter app developer i am creating an news app ',
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 15),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 89,
+                            margin: EdgeInsets.only(left: 170),
+                            decoration: BoxDecoration(
+                              color: Color(0xff3280ef),
+                              borderRadius: BorderRadius.only(topLeft: Radius.circular(20))
+                            ),
+                            child: Icon(Icons.arrow_forward_outlined,size: 30,color: Colors.white,),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                  SizedBox(height: 10,width: 10,),
+                  Material(
+                    elevation: 3,
+                    borderRadius: BorderRadius.circular(15),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10,left: 10),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.white,
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(15))
+                        ),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: Image.asset(
+                                    'assets/img/news1.jpg',
+                                    fit: BoxFit.cover,
+                                    height: 150,
+                                    width: size.width / 1.8,
+                                  ),
+                                )),
+                            SizedBox(height: 5,),
+                            Container(
+                              width: size.width / 1.8,
+                              child: Text(
+                                'I am an flutter app developer i am creating an news app ',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 19),
+                              ),
+                            ),
+                            Container(
+                              width: size.width / 1.8,
+                              child: Text(
+                                'I am an flutter app developer i am creating an news app ',
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 15),
+                              ),
+                            ),
+                            Container(
+                              height: 40,
+                              width: 89,
+                              margin: EdgeInsets.only(left: 170),
+                              decoration: BoxDecoration(
+                                  color: Color(0xff3280ef),
+                                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20))
+                              ),
+                              child: Icon(Icons.arrow_forward_outlined,size: 30,color: Colors.white,),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Screen/Onboarding.dart
+++ lib/Screen/Onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/Screen/Home.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -12,13 +13,59 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: size.height/2,
-            child: Image.asset('assets/img/onboard.jpg'),
-          )
-        ],
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/img/onboard.jpg',
+              fit: BoxFit.cover,
+              height: size.height / 1.7,
+              width: size.width,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: size.height / 1.8),
+              width: size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+              child: Column(children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Get The Latest And Updated \nNews From The App',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset(
+                  'assets/img/newspaper.png',
+                  fit: BoxFit.cover,
+                  height: size.height * .12,
+                  width: size.width * .3,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(onPressed: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>HomeScreen()));
+                },style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  minimumSize: Size(360, 50),
+                ), child: Text('Get Started',style: TextStyle(fontSize: 24),),)
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
